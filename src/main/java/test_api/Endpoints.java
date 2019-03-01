01808a8c4db786ee3210efb43f4e61fc5f2c82de
Roman <r.m.budnik@gmail.com>
2019-03-01 15:07:58 +0200

    accesses updated

--- src/main/java/test_api/Endpoints.java
+++ src/main/java/test_api/Endpoints.java
@@ -1,8 +1,8 @@
 package test_api;
 
-public final class Endpoints {
-    public static final String BASE_URL = "https://reqres.in";
-    public static final String GET_LIST_USERS = "/api/users?page=2";
-    public static final String POST_CREATE = "/api/users";
-    public static final String PUT_PATCH_DELETE = "/api/users/2";
+final class Endpoints {
+    static final String BASE_URL = "https://reqres.in";
+    static final String GET_LIST_USERS = "/api/users?page=2";
+    static final String POST_CREATE = "/api/users";
+    static final String PUT_PATCH_DELETE = "/api/users/2";
 }
--- src/main/java/test_api/HelpClass.java
+++ src/main/java/test_api/HelpClass.java
@@ -2,8 +2,8 @@ package test_api;
 
 import org.json.JSONObject;
 
-public class HelpClass {
-    public static String  createJSON(String key1, String key2, String value1, String value2) {
+class HelpClass {
+    static String  createJSON(String key1, String key2, String value1, String value2) {
         return new JSONObject()
                 .put(key1, value1)
                 .put(key2, value2)
